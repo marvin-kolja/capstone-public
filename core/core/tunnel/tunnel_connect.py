101091055ef499507f99553528ebc9e54ab17cd6
Marvin Willms <marvin.willms@code.berlin>
2024-12-11 08:59:00 +0100

    feat(core): implement `TunnelConnect.start_tunnel`

--- core/core/tunnel/tunnel_connect.py
+++ core/core/tunnel/tunnel_connect.py
@@ -2,9 +2,13 @@ import asyncio
 from typing import Optional
 
 from pydantic import BaseModel, IPvAnyAddress
+from pymobiledevice3.exceptions import PyMobileDevice3Exception
+from pymobiledevice3.lockdown import create_using_usbmux
 from pymobiledevice3.remote.common import TunnelProtocol
-from pymobiledevice3.remote.tunnel_service import TunnelResult as pymobiledevice3TunnelResult
-from pymobiledevice3.tunneld import TunneldCore
+from pymobiledevice3.remote.tunnel_service import TunnelResult as pymobiledevice3TunnelResult, CoreDeviceTunnelProxy
+from pymobiledevice3.tunneld import TunneldCore, TunnelTask
+
+from core.exceptions.tunnel_connect import TunnelAlreadyExistsError
 
 
 class TunnelResult(BaseModel):
@@ -29,11 +33,21 @@ class TunnelConnect:
         self._tunnel_manager: TunneldCore = TunneldCore()
 
     def _start_usbmux_tcp_tunnel_task(self, udid: str, queue: asyncio.Queue) -> asyncio.Task:
-        raise NotImplementedError()
+        lockdown_client = create_using_usbmux(udid)
+        service = CoreDeviceTunnelProxy(lockdown_client)
+        task = asyncio.create_task(
+            self._tunnel_manager.start_tunnel_task(udid, service, protocol=TunnelProtocol.TCP,
+                                                   queue=queue),
+            name=f'start-tunnel-task-{udid}')
+        return task
 
     @staticmethod
     def _parse_pymobiledevice3_tunnel_result(tunnel_result: pymobiledevice3TunnelResult) -> TunnelResult:
-        raise NotImplementedError()
+        return TunnelResult(
+            address=tunnel_result.address,
+            port=tunnel_result.port,
+            protocol=tunnel_result.protocol,
+        )
 
     async def start_tunnel(self, udid: str) -> TunnelResult:
         """
@@ -45,9 +59,31 @@ class TunnelConnect:
 
         :raises TunnelAlreadyExistsError: If a tunnel to the device already exists.
         """
-        # TODO: use `self._start_usbmux_tcp_tunnel_task` to start the tunnel
-        # TODO: use `self._parse_pymobiledevice3_tunnel_result` to parse the tunnel result to our model
-        raise NotImplementedError()
+        if self._tunnel_manager.tunnel_exists_for_udid(udid):
+            raise TunnelAlreadyExistsError(f"Tunnel to {udid} already exists")
+
+        queue = asyncio.Queue()
+
+        try:
+            task = self._start_usbmux_tcp_tunnel_task(udid, queue)
+            self._tunnel_manager.tunnel_tasks[udid] = TunnelTask(task=task, udid=udid)
+
+            tunnel: Optional[pymobiledevice3TunnelResult] = await queue.get()
+
+            if tunnel is not None:
+                return self._parse_pymobiledevice3_tunnel_result(tunnel)
+
+            # If the tunnel is None, the task either failed or was cancelled.
+            # Calling result() will re-raise the exception that caused the task to fail.
+            #
+            # If this raises an InvalidStateError and something is very wrong with the code.
+            try:
+                task.result()
+            except asyncio.CancelledError:
+                pass
+        except PyMobileDevice3Exception as e:
+            # TODO: Wrap PyMobileDevice3Exception in a custom exception
+            raise e
 
     async def stop_tunnel(self, udid: str) -> None:
         """
@@ -72,4 +108,5 @@ class TunnelConnect:
         """
         Close all open tunnels.
         """
-        raise NotImplementedError()
+        # TODO: pass instead of raise, so that test cleanup does not fail for `start_tunnel` method
+        pass
