36667754b62f17f75672897e6b313c36bb615ab7
Marvin Willms <marvin.willms@code.berlin>
2024-12-12 22:01:11 +0100

    feat(core): implement WIP client

---
+++ core/core/tunnel/client.py
@@ -0,0 +1,90 @@
+from datetime import timedelta
+from typing import Optional
+
+from pymobiledevice3.exceptions import DeviceNotFoundError, NoDeviceConnectedError
+
+from core.codec.socket_json_codec import ClientSocketMessageJSONCodec, ClientRequest, ErrorResponse
+from core.exceptions.tunnel_connect import TunnelAlreadyExistsError
+from core.socket import ClientSocket
+from core.tunnel.server_exceptions import ServerErrorCode, InternalServerError, MalformedRequestError, NotFoundError, \
+    TunnelServerErrorCode
+
+
+def get_error_from_context(request: ClientRequest, error_response: ErrorResponse) -> Exception:
+    """
+    Parses the error response and returns the appropriate error. The request is used to get the data that caused the
+    error.
+
+    :param request: The request that caused the error.
+    :param error_response: The error response from the server.
+    :return Exception: The appropriate exception for the error response.
+
+    :raises: ValueError: If the error code in the response is unknown.
+    """
+    error_code = error_response.error_code
+
+    if error_code == ServerErrorCode.INTERNAL:
+        return InternalServerError()
+    elif error_code == ServerErrorCode.MALFORMED_REQUEST:
+        return MalformedRequestError()
+    elif error_code == ServerErrorCode.NOT_FOUND:
+        return NotFoundError()
+    elif error_code == TunnelServerErrorCode.DEVICE_NOT_FOUND:
+        udid = request.data.get('udid')
+        return DeviceNotFoundError(udid=udid)
+    elif error_code == TunnelServerErrorCode.NO_DEVICE_CONNECTED:
+        return NoDeviceConnectedError()
+    elif error_code == TunnelServerErrorCode.TUNNEL_ALREADY_EXISTS:
+        return TunnelAlreadyExistsError()
+    else:
+        raise ValueError(f"Unknown error code: {error_code}")
+
+
+class Client:
+    """
+    A client class to communicate with the `Server`.
+
+    The client is expected to be used as a context manager.
+
+    The class uses a `ClientSocket` to communicate with the server.
+    """
+
+    def __init__(self, port: int, timeout: timedelta):
+        """
+        Initialize the client with the given port.
+
+        This does not connect to the server. The connection is established when the client is used as a context manager.
+
+        :param port: The port to connect to.
+        :param timeout: The timeout for receiving responses.
+        """
+        self._socket: Optional[ClientSocket] = None
+        self._port = port
+        self._timeout = timeout
+
+    def __enter__(self):
+        self._socket = ClientSocket(port=self._port, codec=ClientSocketMessageJSONCodec()).__enter__()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._socket.__exit__(exc_type, exc_val, exc_tb)
+
+    async def _call_server(self, action: str, **kwargs) -> dict:
+        """
+        Call the server with the given action and data.
+
+        :param action: The action/method to call on the server.
+        :param kwargs: any data to send to the server.
+
+        :return: The response data.
+
+        :raises: InvalidSocketMessage: If the request or response are invalid.
+        :raises: Parsed server errors, see `get_error_from_context`.
+        """
+        # TODO:
+        #  1. Create a request with the given action and data.
+        #  2. Send the request to the server.
+        #  3. Receive the response from the server.
+        #  4. If the response is an error, raise the appropriate exception.
+        #  5. Return the response data.
+        raise NotImplementedError
