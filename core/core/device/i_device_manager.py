30ab79d9c1d19ff4d27a2e0329af5fcb8afffd60
Marvin Willms <marvin.willms@code.berlin>
2024-12-18 20:00:31 +0100

    feat(core): abstract browsing of devices to internal method

--- core/core/device/i_device_manager.py
+++ core/core/device/i_device_manager.py
@@ -1,4 +1,5 @@
 from pymobiledevice3 import lockdown, usbmux, exceptions
+from pymobiledevice3.lockdown import LockdownClient
 
 from core.device.i_device import IDevice
 
@@ -7,12 +8,19 @@ class IDeviceManager:
     def __init__(self):
         self.__devices: dict[str, IDevice] = {}
 
+    def _browse_lockdown_clients(self) -> list[LockdownClient]:
+        """
+        Handles logic for browsing available devices as lockdown clients.
+        """
+        raise NotImplementedError()
+
     def list_devices(self) -> list[IDevice]:
         """
-        List all devices found using usbmux
+        Get a list of all available devices as IDevice instances.
 
-        Found devices are stored in memory and refreshed when this method is called again.
+        Discovered devices are stored in memory and refreshed when this method is called again.
         """
+        lockdown_clients = self._browse_lockdown_clients()
         raise NotImplementedError
 
     def get_device(self, udid: str) -> IDevice:
