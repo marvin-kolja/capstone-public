20ca6eb6c40c17c167d7c9cde09aca5bc562fb28
Marvin Willms <marvin.willms@code.berlin>
2024-12-18 02:37:26 +0100

    feat(core): WIP `IDevice`

---
+++ core/core/device/i_device.py
@@ -0,0 +1,259 @@
+from typing import Optional
+
+from packaging.version import Version
+from pymobiledevice3.lockdown import UsbmuxLockdownClient, LockdownClient
+from pymobiledevice3.lockdown_service_provider import LockdownServiceProvider
+from pymobiledevice3.remote.remote_service_discovery import RemoteServiceDiscoveryService
+from pymobiledevice3.services.amfi import AmfiService
+from pymobiledevice3.services.mobile_image_mounter import MobileImageMounterService, DeveloperDiskImageMounter, \
+    PersonalizedImageMounter, auto_mount
+
+from core.exceptions import i_device as device_exceptions
+from core.tunnel.interface import TunnelResult
+
+
+class IDevice:
+    """
+    Wrapper for lockdown client and remote service discovery service.
+    """
+
+    def __init__(self, lockdown_client: UsbmuxLockdownClient):
+        self._lockdown_client = lockdown_client
+        self._rsd: Optional[RemoteServiceDiscoveryService] = None
+
+    @property
+    def lockdown_service(self) -> LockdownServiceProvider:
+        """
+        Returns either the `rsd` or `usbmux_lockdown` property.
+
+        If the `rsd` property is not `None`, it is favored over the `usbmux_lockdown` property.
+
+        Note: The `rsd` property may only be available for devices with iOS 17.0 or later. Refer to the `rsd` property
+        documentation for more information.
+        """
+        if self._rsd is not None:
+            return self._rsd
+        return self._lockdown_client
+
+    @property
+    def lockdown_client(self) -> LockdownClient:
+        """
+        Returns the `LockdownClient` instance.
+
+        If the device is running iOS 17.0 or later, the `rsd` property should be used instead.
+        """
+        return self._lockdown_client
+
+    @property
+    def rsd(self) -> Optional[RemoteServiceDiscoveryService]:
+        """
+        As of iOS 17.0 Apple requires a secure tunnel to access developer services using the `RemoteXPC` protocol.
+
+        For more information read:
+        https://github.com/doronz88/pymobiledevice3/blob/master/misc/understanding_idevice_protocol_layers.md#remotexpc
+
+        The property is `None` if `create_rsd` has never been called.
+
+        **Only available for >= iOS 17.0**
+
+        :raises ValueError: If rsd is not set. Use `establish_trusted_channel` to set the `rsd` property.
+        """
+        if self._rsd is None:
+            raise ValueError('RSD has not been set. Use establish_trusted_channel to set the rsd property')
+        return self._rsd
+
+    @property
+    def requires_tunnel_for_developer_tools(self) -> bool:
+        """
+        Check if the device OS requires a tunnel to access developer services.
+        """
+        return Version(self.lockdown_service.product_version) >= Version('17.0')
+
+    @property
+    def requires_developer_mode(self) -> bool:
+        """
+        Check if the device OS requires developer mode to be enabled in order to mount DDI.
+        """
+        return Version(self.lockdown_service.product_version) >= Version('16.0')
+
+    @property
+    def paired(self) -> bool:
+        return self.lockdown_client.paired
+
+    def check_paired(self):
+        """
+        :raises DeviceNotPaired:
+        """
+        if not self.paired:
+            raise device_exceptions.DeviceNotPaired
+
+    def pair(self):
+        """
+        Pair the device
+
+        :raises UserDeniedPairing:
+        :raises PasswordRequireError:
+        :raises PairingError:
+        """
+        # TODO: Handle exceptions
+        self._lockdown_client.pair()
+
+    def unpair(self):
+        """
+        Unpair the device
+
+        :raises DeviceNotPaired:
+        :raises PairingError:
+        """
+        if not self.paired:
+            raise device_exceptions.DeviceNotPaired
+        # TODO: Handle exceptions
+        self._lockdown_client.unpair()
+
+    @property
+    def developer_mode_enabled(self) -> bool:
+        """
+        **Only available for >= iOS 16**
+
+        :raises DeveloperModeNotSupported:
+        :raises DeveloperModeError:
+
+        :raises DeviceNotPaired:
+        """
+        if not self.requires_developer_mode:
+            raise device_exceptions.DeveloperModeNotSupported
+        self.check_paired()
+        # TODO: Handle exceptions
+        return self._lockdown_client.developer_mode_status
+
+    def check_developer_mode_enabled(self):
+        """
+        :raises DeveloperModeNotSupported:
+
+        :raises DeveloperModeNotEnabled:
+        :raises DeveloperModeError:
+
+        :raises DeviceNotPaired:
+        """
+        if not self.developer_mode_enabled:
+            raise device_exceptions.DeveloperModeNotEnabled
+
+    def enable_developer_mode(self):
+        """
+        Enable Developer Mode on the device
+
+        **Only available for >= iOS 16**
+
+        :raises DeveloperModeNotSupported:
+        :raises DeveloperModeAlreadyEnabled:
+
+        :raises DeviceHasPasscodeSet:
+
+        :raises DeviceNotPaired:
+        """
+        if self.developer_mode_enabled:
+            raise device_exceptions.DeveloperModeAlreadyEnabled
+        # TODO: Handle exceptions
+        AmfiService(self._lockdown_client).enable_developer_mode()
+
+    @property
+    def _mounter(self) -> MobileImageMounterService:
+        if Version(self.lockdown_service.product_version) < Version('17.0'):
+            return DeveloperDiskImageMounter(self.lockdown_service)
+        return PersonalizedImageMounter(self.lockdown_service)
+
+    @property
+    def ddi_mounted(self) -> bool:
+        """
+        :raises DeveloperModeNotEnabled:
+        :raises DeveloperModeError:
+
+        :raises DeviceNotPaired:
+        """
+        try:
+            self.check_developer_mode_enabled()
+        except device_exceptions.DeveloperModeNotSupported:
+            self.check_paired()
+
+        return self._mounter.is_image_mounted(self._mounter.IMAGE_TYPE)
+
+    def check_ddi_mounted(self):
+        """
+        :raises DdiNotMounted:
+
+        :raises DeveloperModeNotEnabled:
+        :raises DeveloperModeError:
+
+        :raises DeviceNotPaired:
+        """
+        if not self.ddi_mounted:
+            raise device_exceptions.DdiNotMounted
+
+    async def mount_ddi(self):
+        """
+        Mount Developer Disk Image
+
+        :raises DdiMountingError:
+
+        :raises DeveloperModeNotEnabled:
+        :raises DeveloperModeError:
+
+        :raises DeviceNotPaired:
+        """
+        if self.ddi_mounted:
+            raise device_exceptions.DdiAlreadyMounted
+
+        # TODO: Handle exceptions
+        await auto_mount(self.lockdown_service)
+
+    def unmount_ddi(self):
+        """
+        Unmount Developer Disk Image
+
+        :raises DdiNotMounted:
+        :raises DdiMountingError:
+
+        :raises DeveloperModeNotEnabled:
+
+        :raises DeviceNotPaired:
+        """
+        self.check_ddi_mounted()
+
+        # TODO: Handle exceptions
+        self._mounter.unmount_image(self._mounter.IMAGE_TYPE)
+
+    async def establish_trusted_channel(self):
+        """
+        Creates a `RemoteServiceDiscoveryService` for the device by establishing a tunnel using `TunnelClient`. This
+        communicates with the `TunnelServer` which is required to run.
+
+        When the tunnel already exists it will reuse the existing tunnel.
+
+        For detailed implementation on how the communication works and tunnels are managed, refer to
+        :mod:`core.tunnel`.
+
+        The method sets the `rsd` property to the created `RemoteServiceDiscoveryService`.
+
+        **Only available for >= iOS 17.0**
+
+        :raises DeviceNotPaired:
+        :raises DeveloperModeNotEnabled:
+        :raises DdiNotMounted:
+        :raises RsdNotSupported:
+        :raises asyncio.TimeoutError:
+        :raises TunnelAlreadyExistsError:
+        :raises TunnelCreationFailure:
+        """
+        # TODO: Check if ready to establish trusted channel
+
+        tunnel: Optional[TunnelResult] = None
+
+        # TODO: Start or get the tunnel
+
+        if tunnel is None:
+            raise device_exceptions.TunnelCreationFailure("Returned tunnel is None")
+
+        rsd = RemoteServiceDiscoveryService((str(tunnel.address), tunnel.port))
+        await rsd.connect()
+
+        self._rsd = rsd
---
+++ core/core/exceptions/i_device.py
@@ -0,0 +1,73 @@
+from core.exceptions import CoreException
+
+
+class PairingError(CoreException):
+    """ Errors related to pairing with the device """
+    pass
+
+
+class UserDeniedPairing(PairingError):
+    """ Raised when pairing fails due to user denying the pairing request """
+    pass
+
+
+class DeviceNotPaired(PairingError):
+    """ Raised when the device is not paired """
+    pass
+
+
+class PasswordRequired(PairingError):
+    """ Raised when a password is required to pair with the device """
+    pass
+
+
+class DeveloperModeError(CoreException):
+    """ Errors related to enabling developer mode """
+    pass
+
+
+class DeveloperModeNotSupported(DeveloperModeError):
+    """ Raised when developer mode is not supported on the device """
+    pass
+
+
+class DeveloperModeAlreadyEnabled(DeveloperModeError):
+    """ Raised when developer mode is already enabled on the device """
+    pass
+
+
+class DeveloperModeNotEnabled(DeveloperModeError):
+    """ Raised when developer mode is not enabled on the device """
+    pass
+
+
+class DeviceHasPasscodeSet(DeveloperModeError):
+    """ We can't enable developer mode if the device has a passcode set """
+    pass
+
+
+class DdiMountingError(CoreException):
+    """ Errors related to mounting Developer Disk Image """
+    pass
+
+
+class DdiNotMounted(DdiMountingError):
+    """ Raised when Developer Disk Image is not mounted """
+    pass
+
+
+class DdiAlreadyMounted(DdiMountingError):
+    """ Raised when Developer Disk Image is already mounted """
+    pass
+
+
+class RsdNotSupported(CoreException):
+    """ Raised when the device is not running iOS 17 or later """
+
+    def __init__(self):
+        super().__init__("RSD is only supported on iOS 17 and later")
+
+
+class TunnelCreationFailure(CoreException):
+    """ Unable to start a tunnel to the device """
+    pass
