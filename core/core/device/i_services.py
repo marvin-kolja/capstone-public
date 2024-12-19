2265edb2788f8d1cf732f0cf9cc961c84cf08081
Marvin Willms <marvin.willms@code.berlin>
2024-12-19 01:59:41 +0100

    feat(core): implement `IServices.install_app`

--- core/core/device/i_services.py
+++ core/core/device/i_services.py
@@ -5,6 +5,7 @@ from pymobiledevice3.services.installation_proxy import InstallationProxyService
 
 from core.device.i_device import IDevice
 from core.device.services_protocol import ServicesProtocol
+from core.exceptions.i_device import AppInstallError
 
 logger = logging.getLogger(__name__)
 
@@ -18,7 +19,20 @@ class IServices(ServicesProtocol):
         return InstallationProxyService(lockdown=self.__device.lockdown_service)
 
     def install_app(self, app_path: str, progress_callback: Callable[[str], None] = None):
-        raise NotImplementedError
+        """
+        Install app onto device.
+
+        :param app_path: Path to ipa.
+        :param progress_callback: Function to call with install progress.
+
+        :raises AppInstallError: If app installation fails.
+        """
+        try:
+            logger.debug(f"Installing app onto device using path: {app_path}")
+            self._installer.install(app_path, handler=progress_callback)
+        except Exception as e:
+            logger.error(f"Failed to install app onto device using path: {app_path}, error: {e}")
+            raise AppInstallError from e
 
     def uninstall_app(self, bundle_id: str, progress_callback: Callable[[str], None] = None):
         raise NotImplementedError
