4bafb38cd67efedaa5f64a7449c38c7e0b4c3d1e
Marvin Willms <marvin.willms@code.berlin>
2025-01-06 11:07:33 +0100

    test(core): add week to timedelta in `timedelta_to_milliseconds` tests

--- core/tests/unit/common/test_timeout.py
+++ core/tests/unit/common/test_timeout.py
@@ -12,11 +12,12 @@ def test_timedelta_to_milliseconds():
     THEN: The timedelta is converted to correct amount of milliseconds taking all timedelta components into account
     AND: The result is an integer
     """
-    timeout = timedelta(days=1, hours=2, minutes=3, seconds=4, milliseconds=5)
+    timeout = timedelta(weeks=1, days=1, hours=2, minutes=3, seconds=4, milliseconds=5)
 
     result = timedelta_to_milliseconds(timeout)
 
-    assert result == 1 * 24 * 60 * 60 * 1000 + \
+    assert result == 1 * 7 * 24 * 60 * 60 * 1000 + \
+           1 * 24 * 60 * 60 * 1000 + \
            2 * 60 * 60 * 1000 + \
            3 * 60 * 1000 + \
            4 * 1000 + \
