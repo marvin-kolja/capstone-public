1b1c8945417d002a90502bff87bfafb080f224b3
Marvin Willms <marvin.willms@code.berlin>
2025-01-06 11:41:14 +0100

    fix(core)!: inconsistency of rounding and return type in timedelta converter
    * Previously, the milliseconds converter, converted seconds (floating) to an int, regardless by truncating the extra precision. On the other hand, the seconds converter kept the extra precision. This has been solved by creating functions that do the first behaviour and functions that keep the extra precision. Those that add extra precision

--- core/core/common/timedelta_converter.py
+++ core/core/common/timedelta_converter.py
@@ -1,9 +1,53 @@
 from datetime import timedelta
 
 
-def timedelta_to_seconds(delta: timedelta) -> float:
+def timedelta_to_seconds_precise(delta: timedelta) -> float:
+    """
+    Converts a timedelta object to seconds.
+
+    The result may be a floating-point number for subsecond microsecond precision.
+
+    :param delta: The timedelta object to convert
+    :return: Floating-point number representing the seconds
+    """
+
     return delta.total_seconds()
 
 
+def timedelta_to_seconds(delta: timedelta) -> int:
+    """
+    Converts a timedelta object to seconds.
+
+    Microseconds are truncated
+
+    :param delta: The timedelta object to convert
+
+    :return: Integer representing the seconds
+    """
+    return int(timedelta_to_seconds_precise(delta))
+
+
+def timedelta_to_milliseconds_precise(delta: timedelta) -> float:
+    """
+    Converts a timedelta object to milliseconds.
+
+    The result may be a floating-point number for submillisecond microsecond precision.
+
+    :param delta: The timedelta object to convert
+
+    :return: Floating-point number representing the milliseconds
+    """
+    return timedelta_to_seconds_precise(delta) * 1e3
+
+
 def timedelta_to_milliseconds(delta: timedelta) -> int:
-    return int(timedelta_to_seconds(delta) * 1e3)
+    """
+    Converts a timedelta object to milliseconds.
+
+    Microseconds are truncated
+
+    :param delta: The timedelta object to convert
+
+    :return: Integer representing the milliseconds
+    """
+    return int(timedelta_to_milliseconds_precise(delta))
--- core/tests/unit/common/test_timedelta_converter.py
+++ core/tests/unit/common/test_timedelta_converter.py
@@ -1,18 +1,44 @@
 from datetime import timedelta
 
-from core.common.timedelta_converter import timedelta_to_milliseconds, timedelta_to_seconds
+from core.common.timedelta_converter import timedelta_to_milliseconds, timedelta_to_seconds, \
+    timedelta_to_milliseconds_precise, timedelta_to_seconds_precise
+
+
+def test_timedelta_to_milliseconds_precise():
+    """
+    GIVEN: A timedelta object
+
+    WHEN: Calling `timedelta_to_milliseconds_precise`
+
+    THEN: The timedelta is converted to correct amount of milliseconds taking all timedelta components into account
+    AND: The result is a float with microsecond precision
+    """
+    timeout = timedelta(weeks=1, days=1, hours=2, minutes=3, seconds=4, milliseconds=5, microseconds=6)
+
+    result = timedelta_to_milliseconds_precise(timeout)
+
+    assert result == 1 * 7 * 24 * 60 * 60 * 1000 + \
+           1 * 24 * 60 * 60 * 1000 + \
+           2 * 60 * 60 * 1000 + \
+           3 * 60 * 1000 + \
+           4 * 1000 + \
+           5 + \
+           6 / 1e3
+
+    assert isinstance(result, float)
 
 
 def test_timedelta_to_milliseconds():
     """
-    GIVEN: A timedelta object
+    GIVEN: A timedelta object with microseconds more than 500
 
     WHEN: Calling `timedelta_to_milliseconds`
 
     THEN: The timedelta is converted to correct amount of milliseconds taking all timedelta components into account
+    AND: Microseconds are NOT rounded, but truncated
     AND: The result is an integer
     """
-    timeout = timedelta(weeks=1, days=1, hours=2, minutes=3, seconds=4, milliseconds=5)
+    timeout = timedelta(weeks=1, days=1, hours=2, minutes=3, seconds=4, milliseconds=5, microseconds=501)
 
     result = timedelta_to_milliseconds(timeout)
 
@@ -26,16 +52,42 @@ def test_timedelta_to_milliseconds():
     assert isinstance(result, int)
 
 
-def test_timedelta_to_seconds():
+def test_timedelta_to_seconds_precise():
     """
     GIVEN: A timedelta object
 
-    WHEN: Calling `timedelta_to_seconds`
+    WHEN: Calling `timedelta_to_seconds_precise`
 
     THEN: The timedelta is converted to correct amount of seconds taking all timedelta components into account
     AND: The result is a float
     """
-    timeout = timedelta(weeks=1, days=1, hours=2, minutes=3, seconds=4, milliseconds=5)
+    timeout = timedelta(weeks=1, days=1, hours=2, minutes=3, seconds=4, milliseconds=5, microseconds=6)
+
+    result = timedelta_to_seconds_precise(timeout)
+
+    assert result == \
+           1 * 7 * 24 * 60 * 60 + \
+           1 * 24 * 60 * 60 + \
+           2 * 60 * 60 + \
+           3 * 60 + \
+           4 + \
+           5 / 1e3 + \
+           6 / 1e6
+
+    assert isinstance(result, float)
+
+
+def test_timedelta_to_seconds():
+    """
+    GIVEN: A timedelta object with milliseconds more than 500
+
+    WHEN: Calling `timedelta_to_seconds`
+
+    THEN: The timedelta is converted to correct amount of seconds taking all timedelta components into account
+    AND: Microseconds are NOT rounded, but truncated
+    AND: The result is an integer
+    """
+    timeout = timedelta(weeks=1, days=1, hours=2, minutes=3, seconds=4, milliseconds=500, microseconds=6)
 
     result = timedelta_to_seconds(timeout)
 
@@ -44,7 +96,6 @@ def test_timedelta_to_seconds():
            1 * 24 * 60 * 60 + \
            2 * 60 * 60 + \
            3 * 60 + \
-           4 + \
-           5 / 1000
+           4
 
-    assert isinstance(result, float)
+    assert isinstance(result, int)
