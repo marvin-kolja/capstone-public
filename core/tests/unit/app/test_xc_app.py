6fcc2c07c1e2738a779e560ddf973d8e25447c91
Marvin Willms <marvin.willms@code.berlin>
2025-01-07 11:46:20 +0100

    test(core): add failing test for parsing apps 'Info.plist' file

---
+++ core/tests/unit/app/test_xc_app.py
@@ -0,0 +1,32 @@
+import os
+import pathlib
+import shutil
+
+from core.app.xc_app import XcApp
+
+
+class TestXcApp:
+    def test_parse_info_plist(self, tmp_path, example_info_plist_path):
+        """
+        GIVEN: A path to an .app package.
+        AND: A valid Info.plist file in the .app package.
+
+        WHEN: The `parse_info_plist` method is called.
+
+        THEN: The Info.plist file is read and parsed correctly.
+        """
+        fake_app_path = pathlib.Path(tmp_path, "Placeholder.app")
+        fake_app_path.mkdir()
+        # Copy file example `Info.plist` to the fake app path.
+        shutil.copyfile(example_info_plist_path, fake_app_path.joinpath("Info.plist"))
+
+        app = XcApp(fake_app_path.absolute().as_posix())
+
+        info_plist = app.parse_info_plist()
+
+        assert info_plist.CFBundlePackageType == "APPL"
+        assert info_plist.CFBundleIdentifier == "com.example.Placeholder"
+        assert info_plist.CFBundleName == "Placeholder"
+        assert info_plist.CFBundleDisplayName is None
+        assert info_plist.MinimumOSVersion == "17.3"
+        assert info_plist.LSRequiresIPhoneOS is True
