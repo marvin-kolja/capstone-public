f88680eeadbeea2a088be51ffa39d630404518a6
Marvin Willms <marvin.willms@code.berlin>
2025-01-07 11:47:52 +0100

    feat(core): implement parsing info plist file inside .app package

--- core/core/app/xc_app.py
+++ core/core/app/xc_app.py
@@ -1,6 +1,8 @@
 import logging
+import pathlib
 
 from core.app.info_plist import InfoPlist
+from core.common.plist_reader import read_plist
 
 logger = logging.getLogger(__name__)
 
@@ -25,4 +27,15 @@ class XcApp:
 
         :raises: `FileNotFoundError` when the Info.plist file is not found.
         """
-        raise NotImplementedError
+        info_plist_path = pathlib.Path(self._path, "Info.plist").absolute().as_posix()
+
+        if not pathlib.Path(info_plist_path).exists():
+            logger.warning(f"Info.plist file not found at path: {info_plist_path}")
+            raise FileNotFoundError(
+                f"Info.plist file not found at path: {info_plist_path}"
+            )
+
+        data = read_plist(pathlib.Path(self._path, "Info.plist").absolute().as_posix())
+        logger.debug(f"Successfully read Info.plist file from path: {info_plist_path}")
+
+        return InfoPlist.model_validate(data)
