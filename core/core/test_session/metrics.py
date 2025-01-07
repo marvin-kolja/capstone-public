e038a59ec34f58b51a274f00472afb0c78fc97a5
Marvin Willms <marvin.willms@code.berlin>
2025-01-07 08:51:02 +0100

    feat(core): parse metrics to instruments

--- core/core/test_session/metrics.py
+++ core/core/test_session/metrics.py
@@ -1,5 +1,7 @@
 from enum import StrEnum
 
+from core.subprocesses.xctrace_command import Instrument
+
 
 class Metric(StrEnum):
     """
@@ -10,3 +12,41 @@ class Metric(StrEnum):
     memory = "memory"
     fps = "fps"
     gpu = "gpu"
+
+
+def parse_metric_to_instrument(metric: Metric) -> Instrument:
+    """
+    Parses a metric to an instrument.
+    :param metric: the metric to parse
+    :return: the instrument
+
+    :raises ValueError: if the metric is invalid
+    """
+    if metric == Metric.cpu:
+        return Instrument.activity_monitor
+    elif metric == Metric.memory:
+        return Instrument.activity_monitor
+    elif metric == Metric.fps:
+        return Instrument.core_animation_fps
+    elif metric == Metric.gpu:
+        return Instrument.core_animation_fps
+    else:
+        raise ValueError(f"Invalid metric: {metric}")
+
+
+def parse_metrics_to_instruments(metrics: list[Metric]) -> list[Instrument]:
+    """
+    Parses a list of metrics to a list of instruments.
+
+    :param metrics: the metrics to parse
+    :return: the instruments
+
+    :raises ValueError: if any metric is invalid
+    """
+    instrument_set = set()
+
+    for metric in metrics:
+        instrument = parse_metric_to_instrument(metric)
+        instrument_set.add(instrument)
+
+    return list(instrument_set)
