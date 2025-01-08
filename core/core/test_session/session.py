8e10acfc265e6ef699e4b2e84591c9e56f7f39c6
Marvin Willms <marvin.willms@code.berlin>
2025-01-08 11:14:39 +0100

    feat(core): wip `Session` & `SessionState`

---
+++ core/core/test_session/session.py
@@ -0,0 +1,72 @@
+import logging
+from typing import Optional
+from uuid import UUID
+
+from core.device.i_device import IDevice
+from core.device.i_services import IServices
+from core.test_session.execution_plan import ExecutionPlan
+from core.test_session.session_state import SessionState
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+class Session:
+    """
+    This class is responsible for running the test session.
+
+    Attributes:
+        _execution_plan (ExecutionPlan): The execution plan for the test session.
+        _session_id (UUID): The unique identifier for the test session.
+        _device (IDevice): The device to run the tests on.
+        _output_dir (str): The directory to store the trace and xcresult files.
+        _i_services (IServices): The services to interact with the device.
+        _session_state (Optional[SessionState]): The state of the test session.
+    """
+
+    def __init__(
+        self,
+        execution_plan: ExecutionPlan,
+        session_id: UUID,
+        device: IDevice,
+        output_dir: str,
+    ):
+        """
+        :param execution_plan: The execution plan for the test session.
+        :param session_id: The unique identifier for the test session.
+        :param device: The device to run the tests on.
+        """
+        self._execution_plan = execution_plan
+        self._session_id = session_id
+        self._device = device
+        self._output_dir = output_dir
+        self._i_services = IServices(self._device)
+        self._session_state = SessionState(
+            execution_plan=self._execution_plan,
+            session_id=self._session_id,
+        )
+
+    async def run(self):
+        """
+        This will prepare the test session and then run the steps in the test plan.
+        """
+        await self._prepare()
+        await self._run_execution_plan()
+
+    async def _prepare(self):
+        """
+        Prepare the test session.
+
+        This will:
+
+        - Check if the execution plan is valid.
+        - Check if the device is ready for testing.
+        """
+        if self._execution_plan.execution_steps is None:
+            raise ValueError("Execution plan is not planned.")
+        self._device.check_dvt_ready()
+
+    async def _run_execution_plan(self):
+        """
+        Uses the execution plan to execute the execution steps one by one.
+        """
+        raise NotImplementedError
---
+++ core/core/test_session/session_state.py
@@ -0,0 +1,69 @@
+from typing import Optional, Literal
+from uuid import UUID
+
+from core.test_session.execution_plan import ExecutionStep, ExecutionPlan
+
+
+class ExecutionStepState:
+    """
+    This contains the state of an execution step. It stores the current status and other information about the execution
+    step.
+
+    Attributes:
+        _execution_step:
+        _status: The current status of the execution step.
+        _exception: The exception that occurred during the execution step.
+    """
+
+    def __init__(self, execution_step: ExecutionStep):
+        self._execution_step = execution_step
+        self._status: Literal["not_started", "running", "completed", "failed"] = (
+            "not_started"
+        )
+        self._exception: Optional[Exception] = None
+
+    @property
+    def status(self) -> Literal["not_started", "running", "completed", "failed"]:
+        return self._status
+
+    @property
+    def execution_step(self) -> ExecutionStep:
+        return self._execution_step
+
+    @property
+    def exception(self) -> Optional[Exception]:
+        return self._exception
+
+    def set_running(self):
+        self._status = "running"
+
+    def set_completed(self):
+        self._status = "completed"
+
+    def set_failed(self, exception: Exception):
+        self._status = "failed"
+        self._exception = exception
+
+
+class SessionState:
+    """
+    This class keeps track of the current state of the test session. It is responsible for managing the execution steps
+    and also stores the execution step state (``ExecutionStepState``).
+    """
+
+    def __init__(self, execution_plan: ExecutionPlan, session_id: UUID):
+        """
+        :param execution_plan: The execution plan.
+        :param session_id: The unique identifier for the test session.
+        """
+        self._execution_plan = execution_plan
+        self._session_id = session_id
+        self._execution_step_states: dict[str, ExecutionStepState] = {}
+        self._current_execution_step_index = -1
+
+    @property
+    def total_execution_steps(self) -> int:
+        """
+        Gets the total number of execution
+        """
+        return len(self._execution_plan.execution_steps)
