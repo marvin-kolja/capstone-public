89f5427c3237ba8e2a7c598d287213470b3f0364
Marvin Willms <marvin.willms@code.berlin>
2025-01-08 11:08:43 +0100

    feat(core): wip session and execution step hasher

---
+++ core/core/test_session/session_step_hasher.py
@@ -0,0 +1,20 @@
+from uuid import UUID
+
+from core.hasher import Hasher
+from core.test_session.execution_plan import ExecutionStep
+
+
+def hash_session_execution_step(session_id: UUID, execution_step: ExecutionStep):
+    """
+    Generate a hash for an execution step and the session id. This hash can be used to uniquely identify the execution
+    step in e.g. a database or filesystem.
+
+    Uses :meth:`core.hasher.Hasher.hash` to hash a string representing the dynamic parts of the execution step into a
+    hexadecimal format.
+
+    :param session_id: the test session id
+    :param execution_step: the execution step
+
+    :return: the hash of a string representing the dynamic parts of the execution step
+    """
+    return Hasher.hash("")
