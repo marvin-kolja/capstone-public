e963fe46b1beac157fa4ce21cfb12928fe2dbd9d
Marvin Willms <marvin.willms@code.berlin>
2025-01-08 21:23:17 +0100

    feat(core): implement `Session._run_execution_step`

--- core/core/test_session/session.py
+++ core/core/test_session/session.py
@@ -1,11 +1,21 @@
+import asyncio
 import logging
+from contextlib import suppress
+from datetime import timedelta
 from typing import Optional
 from uuid import UUID
 
 from core.device.i_device import IDevice
 from core.device.i_services import IServices
+from core.subprocesses.process import ProcessException
+from core.subprocesses.xcodebuild_command import IOSDestination
+from core.subprocesses.xctrace_command import Instrument
 from core.test_session.execution_plan import ExecutionPlan, ExecutionStep
-from core.test_session.session_state import SessionState, ExecutionStepState
+from core.test_session.metrics import parse_metrics_to_instruments
+from core.test_session.session_state import SessionState
+from core.test_session.session_step_hasher import hash_session_execution_step
+from core.test_session.xctest import Xctest
+from core.test_session.xctrace import Xctrace
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -87,6 +97,108 @@ class Session:
         """
         Run an execution step.
 
+        1. Uninstall the apps if required
+        2. Install the apps if not already installed, regardless of the reinstall_app flag
+        3. Parse metrics to instruments
+        4. Create paths for trace and xcresult files.
+        5. Execute the test cases and record the metrics.
+           - The order of how those are started depends on the recording_start_strategy.
+           - Either way, both run in parallel in the end.
+
         :param execution_step: The execution step to run.
         """
-        raise NotImplementedError
+        app_path = execution_step.test_target.app_path
+        ui_test_app_path = execution_step.test_target.ui_test_app_path
+        app_paths = [app_path, ui_test_app_path] if ui_test_app_path else [app_path]
+
+        installed_apps = self._i_services.list_installed_apps()
+
+        for path_to_app in app_paths:
+            bundle_id = self._execution_plan.info_plists[path_to_app].CFBundleIdentifier
+
+            if bundle_id in installed_apps and execution_step.reinstall_app:
+                self._i_services.uninstall_app(bundle_id)
+                self._i_services.install_app(app_path)
+            elif bundle_id not in installed_apps:
+                self._i_services.install_app(app_path)
+
+        app_bundle_id = self._execution_plan.info_plists[app_path].CFBundleIdentifier
+
+        instruments: list[Instrument] = parse_metrics_to_instruments(
+            execution_step.metrics
+        )
+
+        trace_path = f"{self._output_dir}/{hash_session_execution_step(self._session_id, execution_step)}.trace"
+        xcresult_path = f"{self._output_dir}/{hash_session_execution_step(self._session_id, execution_step)}.xcresult"
+
+        test_task = None
+        trace_task = None
+
+        test_task_builder = lambda: Xctest.run_test(
+            xcresult_path=xcresult_path,
+            test_configuration=self._execution_plan.test_plan.xctestrun_config.test_configuration,
+            xctestrun_path=self._execution_plan.test_plan.xctestrun_config.path,
+            only_testing=[
+                test_case.xctest_id for test_case in execution_step.test_cases
+            ],
+            destination=IOSDestination(id=self._device.lockdown_service.udid),
+        )
+
+        try:
+            if execution_step.recording_start_strategy == "launch":
+                trace_task = asyncio.create_task(
+                    Xctrace.record_launch(
+                        trace_path=trace_path,
+                        instruments=instruments,
+                        app_to_launch=app_bundle_id,
+                        append_trace=False,
+                        device=self._device.lockdown_service.udid,
+                    )
+                )
+                await self._i_services.wait_for_app_pid(app_bundle_id)
+                test_task = asyncio.create_task(test_task_builder())
+            elif execution_step.recording_start_strategy == "attach":
+                test_task = asyncio.create_task(test_task_builder())
+                pid = await self._i_services.wait_for_app_pid(
+                    app_bundle_id, frequency=timedelta(milliseconds=500)
+                )
+                trace_task = asyncio.create_task(
+                    Xctrace.record_attach(
+                        trace_path=trace_path,
+                        instruments=instruments,
+                        pid=pid,
+                        append_trace=False,
+                        device=self._device.lockdown_service.udid,
+                    )
+                )
+            else:
+                raise ValueError(
+                    f"Invalid recording start strategy: {execution_step.recording_start_strategy}"
+                )
+
+            # Await both tasks but cancel the trace task if the test task fails or ends
+            tasks = await asyncio.wait(
+                [trace_task, test_task], return_when=asyncio.FIRST_COMPLETED
+            )
+
+            if test_task in tasks[0]:
+                if test_task.exception():
+                    trace_task.cancel()
+                    await trace_task
+                    raise test_task.exception()
+            if trace_task in tasks[0]:
+                if trace_task.exception():
+                    test_task.cancel()
+                    await test_task
+                    raise trace_task.exception()
+        except Exception as e:
+            raise
+        finally:
+            if test_task:
+                test_task.cancel()
+                with suppress(asyncio.CancelledError, ProcessException):
+                    await test_task
+            if trace_task:
+                trace_task.cancel()
+                with suppress(asyncio.CancelledError, ProcessException):
+                    await trace_task
