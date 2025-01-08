744f3bb916df81965a997b05c8437ea0f83ceda2
Marvin Willms <marvin.willms@code.berlin>
2025-01-08 08:43:25 +0100

    feat(core): add logging to `ExecutionPlan`

--- core/core/test_session/execution_plan.py
+++ core/core/test_session/execution_plan.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Literal, Optional
 
 from pydantic import BaseModel
@@ -8,6 +9,8 @@ from core.test_session.metrics import Metric
 from core.test_session.plan import PlanStep, StepTestCase, SessionTestPlan
 from core.test_session.xctest import Xctest
 from core.test_session.xctestrun import XcTestTarget, Xctestrun, XcTestConfiguration
+
+logger = logging.getLogger(__name__)
 
 
 class ExecutionStep(BaseModel):
@@ -68,14 +71,45 @@ class ExecutionPlan:
         * Extract the info plists
         * Plan the execution steps
         """
-        xctestrun_config = self.test_plan.xctestrun_config
-        self.xctestrun = Xctest.parse_xctestrun(xctestrun_config.path)
-        xc_test_configuration = self.xctestrun.extract_test_configuration_with_name(
-            xctestrun_config.test_configuration
+        logger.info(
+            f"Starting to plan execution steps for the test plan: {self.test_plan.name}"
         )
+        xctestrun_config = self.test_plan.xctestrun_config
+        xctestrun_path = xctestrun_config.path
+        x_test_configuration_name = xctestrun_config.test_configuration
+
+        self.xctestrun = Xctest.parse_xctestrun(xctestrun_path)
+        logger.debug(
+            f"Xctestrun file parsed: {xctestrun_path} for test plan {self.test_plan.name}"
+        )
+
+        try:
+            xc_test_configuration = self.xctestrun.extract_test_configuration_with_name(
+                x_test_configuration_name
+            )
+        except ValueError:
+            logger.error(
+                f"Test configuration '{x_test_configuration_name}' not found in xctestrun file {xctestrun_path}"
+            )
+            raise
+        logger.debug(
+            f"Test configuration '{x_test_configuration_name}' extracted from xctestrun file {xctestrun_path}"
+        )
+
         self.xc_test_targets = self._convert_test_targets_to_dict(xc_test_configuration)
+
+        logger.debug(
+            f"Extracted test targets from xctestrun file {xctestrun_path} using test configuration "
+            f"'{x_test_configuration_name}': {list(self.xc_test_targets.keys())}"
+        )
+
         self.info_plists = self._extract_info_plists(
             list(self.xc_test_targets.values())
+        )
+
+        logger.debug(
+            f"Extracted Info plists from xctestrun file {xctestrun_path} for test targets: "
+            f"{list(self.info_plists.keys())}"
         )
 
         self.execution_steps = self._generate_execution_steps(
@@ -90,12 +124,24 @@ class ExecutionPlan:
         Plan the execution steps based on the test plan. This will generate a list of `ExecutionStep` that
         represent the steps to be executed in the test session.
         """
+        logger.info(
+            f"Generating execution steps for test plan: {test_plan.name} with {len(test_plan.steps)} steps"
+        )
+
         execution_steps: list[ExecutionStep] = []
 
         if test_plan.repetition_strategy == "entire_suite":
+            logger.debug(
+                f"Repetition strategy is 'entire_suite', generating execution steps for the entire suite"
+            )
+
             for repetition in range(test_plan.repetitions):
                 for step in test_plan.steps:
                     for step_repetition in range(step.repetitions):
+                        logger.debug(
+                            f"Generating execution steps for step '{step.name}' with plan repetition '{repetition}' and "
+                            f"step repetition '{step_repetition}'"
+                        )
                         execution_steps.extend(
                             ExecutionPlan._generate_plan_step_execution_steps(
                                 test_plan=test_plan,
@@ -106,9 +152,16 @@ class ExecutionPlan:
                             )
                         )
         elif test_plan.repetition_strategy == "per_step":
+            logger.debug(
+                f"Repetition strategy is 'per_step', generating execution steps for each step"
+            )
+
             for step in test_plan.steps:
                 repetitions = step.repetitions * test_plan.repetitions
                 for current_repetition in range(repetitions):
+                    logger.debug(
+                        f"Generating execution steps for step '{step.name}' with repetition {current_repetition}"
+                    )
                     execution_steps.extend(
                         ExecutionPlan._generate_plan_step_execution_steps(
                             test_plan=test_plan,
@@ -119,9 +172,13 @@ class ExecutionPlan:
                         )
                     )
         else:
-            raise ValueError(
-                f"Invalid repetition strategy: {test_plan.repetition_strategy}"
-            )
+            err_msg = f"Invalid repetition strategy: {test_plan.repetition_strategy} for test plan {test_plan.name}"
+            logger.error(err_msg)
+            raise ValueError(err_msg)
+
+        logger.info(
+            f"Generated {len(execution_steps)} execution steps for test plan: {test_plan.name}"
+        )
         return execution_steps
 
     @staticmethod
@@ -149,6 +206,9 @@ class ExecutionPlan:
         recording_start_strategy: Literal["launch", "attach"] = (
             step.recording_start_strategy or test_plan.recording_start_strategy
         )
+        logger.debug(
+            f"Recording start strategy for step '{step.name}' is '{recording_start_strategy}'"
+        )
 
         # Determine correct reinstall app strategy
         reinstall_app = (
@@ -156,20 +216,28 @@ class ExecutionPlan:
             if step.reinstall_app is not None
             else test_plan.reinstall_app
         )
+        logger.debug(
+            f"Reinstall app strategy for step '{step.name}' is '{reinstall_app}'"
+        )
 
         # Determine correct metrics
         metrics = step.metrics if step.metrics is not None else test_plan.metrics
+        logger.debug(f"Metrics for step '{step.name}' are '{metrics}'")
 
         # NOTE: We assume that all test cases in a step are from the same test target here as test cases are validated
         # that way. Thus, we can safely get the test target name from the first test case.
         test_target_name = step.test_cases[0].test_target
+        logger.debug(f"Test target name for step '{step.name}' is '{test_target_name}'")
 
         if (test_target := xc_test_targets.get(test_target_name)) is None:
-            raise ValueError(
-                f"Test target '{test_target_name}' not found in xctestrun file {test_plan.xctestrun_config.path}"
-            )
+            err_msg = f"Test target '{test_target_name}' not found in xctestrun file {test_plan.xctestrun_config.path}"
+            logger.error(err_msg)
+            raise ValueError(err_msg)
 
         if recording_strategy == "per_step":
+            logger.debug(
+                f"Recording strategy is 'per_step', adding a single execution step for the entire step '{step.name}'"
+            )
             execution_steps.append(
                 ExecutionStep(
                     plan_repetition=repetition,
@@ -184,11 +252,18 @@ class ExecutionPlan:
                 )
             )
         elif recording_strategy == "per_test":
+            logger.debug(
+                f"Recording strategy is 'per_test', looping through each test case in the step"
+            )
+
             for idx, test_case in enumerate(step.test_cases):
                 first_in_step = idx == 0
                 should_reinstall_app = first_in_step and reinstall_app
                 # Ensure the app is reinstalled ONLY once per step, if required
 
+                logger.debug(
+                    f"Adding execution step for test case '{test_case.xctest_id}' of step '{step.name}'"
+                )
                 execution_steps.append(
                     ExecutionStep(
                         plan_repetition=repetition,
@@ -203,8 +278,13 @@ class ExecutionPlan:
                     )
                 )
         else:
-            raise ValueError(f"Invalid recording strategy: {recording_strategy}")
+            err_msg = f"Invalid recording strategy: {recording_strategy}"
+            logger.error(err_msg)
+            raise ValueError(err_msg)
 
+        logger.debug(
+            f"Generated {len(execution_steps)} execution steps for step '{step.name}'"
+        )
         return execution_steps
 
     @staticmethod
@@ -216,11 +296,16 @@ class ExecutionPlan:
 
         :param xc_test_configuration: The test configuration to extract the test targets from.
         """
+        logger.debug(f"Converting test targets to dictionary")
+
         test_targets: dict[str, XcTestTarget] = {}
 
         for test_target in xc_test_configuration.TestTargets:
             # BlueprintName is the name of the test target and can be used to identify if xctest ids have a valid test
             # suite name.
+            logger.debug(
+                f"Adding test target: {test_target.BlueprintName} to dictionary"
+            )
             test_targets[test_target.BlueprintName] = test_target
 
         return test_targets
@@ -233,20 +318,33 @@ class ExecutionPlan:
 
         :param test_targets: The test targets to extract the info plists from.
         """
+        logger.debug(f"Extracting Info plists for test targets: {test_targets}")
 
         info_plists: dict[str, InfoPlist] = {}
 
         for test_target in test_targets:
+            logger.debug(f"Extracting Info plists for test target: {test_target}")
+
             target_app = XcApp(test_target.app_path)
 
             if target_app.path not in info_plists:
+                logger.debug(f"Extracting Info plist for app: {target_app.path}")
                 info_plist = target_app.parse_info_plist()
                 info_plists[target_app.path] = info_plist
+            else:
+                logger.debug(f"Info plist for app: {target_app.path} already extracted")
 
             if test_target.ui_test_app_path is not None:
                 ui_test_app = XcApp(test_target.ui_test_app_path)
                 if ui_test_app.path not in info_plists:
+                    logger.debug(
+                        f"Extracting Info plist for UI test app: {ui_test_app.path}"
+                    )
                     ui_info_plist = ui_test_app.parse_info_plist()
                     info_plists[ui_test_app.path] = ui_info_plist
+                else:
+                    logger.debug(
+                        f"Info plist for UI test app: {ui_test_app.path} already extracted"
+                    )
 
         return info_plists
