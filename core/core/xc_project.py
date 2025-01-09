286b811cd021d057de737d9347d2e4268e511168
Marvin Willms <marvin.willms@code.berlin>
2025-01-09 12:40:20 +0100

    feat(core): wip `XcProject` class to interact with xcode projects

---
+++ core/core/xc_project.py
@@ -0,0 +1,76 @@
+import logging
+
+from pydantic import BaseModel
+
+logger = logging.getLogger(__name__)
+
+
+class ProjectDetails(BaseModel):
+    """
+    Details of an xcode project.
+    """
+
+    configurations: list[str]
+    name: str
+    schemes: list[str]
+    targets: list[str]
+
+
+class XcListProjectResult(BaseModel):
+    """
+    Result of listing an xcode project.
+    """
+
+    project: ProjectDetails
+
+
+class XcShowTestPlansResult(BaseModel):
+    """
+    Test plans result from `xcodebuild -showTestPlans -json` command.
+    """
+
+    class __XcTestPlan(BaseModel):
+        """
+        Xcode test plan.
+        """
+
+        name: str
+
+    testPlans: list[__XcTestPlan]
+
+
+class XcProject:
+    """
+    Methods to interact with an Xcode project.
+    """
+
+    def __init__(self, path_to_project: str):
+        if not path_to_project.endswith(".xcodeproj"):
+            error_msg = (
+                f"Path to project must end with .xcodeproj, but got {path_to_project}"
+            )
+            logger.error(error_msg)
+            raise ValueError(
+                error_msg,
+            )
+        self.path_to_project = path_to_project
+
+    async def list(self) -> ProjectDetails:
+        """
+        Lists the targets, configurations, and schemes of the project.
+
+        :return: Project details
+        :raises ProcessException: when the executed command fails
+        :raises ValidationError: when the output of the command cannot be parsed correctly
+        """
+        raise NotImplementedError
+
+    async def xcode_test_plans(self) -> [str]:
+        """
+        Get the test plans from the given project.
+
+        :return: Names of the test plans as a list
+        :raises ProcessException: when the executed command fails
+        :raises ValidationError: when the output of the command cannot be parsed correctly
+        """
+        raise NotImplementedError
