a55c05eccb59f6796d7b74d99034168f31a2b8a8
Marvin Willms <marvin.willms@code.berlin>
2025-01-10 13:22:26 +0100

    feat(core): add command parser for xcresulttool with test results' tests and summary methods

---
+++ core/core/subprocesses/xcresult_command.py
@@ -0,0 +1,36 @@
+from core.subprocesses.process import ProcessCommand
+
+
+class XcresultToolCommand(ProcessCommand):
+    """
+    A command parser to interact with the xcresulttool command.
+    """
+
+    def __init__(self, xcresult_path: str, get_options: list[str]):
+        self.xcresult_path = xcresult_path
+        self.get_options = get_options
+
+    def parse(self) -> list[str]:
+        return [
+            "xcrun",
+            "xcresulttool",
+            "get",
+            *self.get_options,
+            "--path",
+            self.xcresult_path,
+            "--compact",  # This way the output is just one line
+        ]
+
+    @classmethod
+    def get_tests_structure(cls, xcresult_path: str):
+        """
+        Gets the tests structure from the test results.
+        """
+        return cls(xcresult_path, ["test-results", "tests"])
+
+    @classmethod
+    def get_test_result_summary(cls, xcresult_path: str):
+        """
+        Gets the summary from the test results.
+        """
+        return cls(xcresult_path, ["test-results", "summary"])
---
+++ core/tests/unit/subprocess/test_xcresult_command.py
@@ -0,0 +1,60 @@
+from core.subprocesses.xcresult_command import XcresultToolCommand
+from tests.conftest import fake_udid
+
+
+class TestXcresultToolCommand:
+    """
+    Tests for the xcresulttool command
+    """
+
+    def test_get_tests_structure(self, fake_udid):
+        """
+        GIVEN: A XcresultToolCommand class
+
+        WHEN: calling `get_tests_structure`
+        AND: then calling `parse` on the returned command
+
+        THEN: The correct list of strings should be returned
+        """
+        command = XcresultToolCommand.get_tests_structure(
+            xcresult_path="/tmp/test_results.xcresult",
+        )
+
+        parsed_command = command.parse()
+
+        assert parsed_command == [
+            "xcrun",
+            "xcresulttool",
+            "get",
+            "test-results",
+            "tests",
+            "--path",
+            "/tmp/test_results.xcresult",
+            "--compact",
+        ]
+
+    def test_get_test_result_summary(self, fake_udid):
+        """
+        GIVEN: A XctraceCommand class
+
+        WHEN: calling `get_test_result_summary`
+        AND: then calling `parse` on the returned command
+
+        THEN: The correct list of strings should be returned
+        """
+        command = XcresultToolCommand.get_test_result_summary(
+            xcresult_path="/tmp/test_results.xcresult",
+        )
+
+        parsed_command = command.parse()
+
+        assert parsed_command == [
+            "xcrun",
+            "xcresulttool",
+            "get",
+            "test-results",
+            "summary",
+            "--path",
+            "/tmp/test_results.xcresult",
+            "--compact",
+        ]
