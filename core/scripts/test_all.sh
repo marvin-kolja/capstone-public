99a793d0cab4fae104e63c36d247bcd35d65148a
Marvin Willms <marvin.willms@code.berlin>
2025-01-12 22:27:54 +0100

    chore(core): add additional options to test script

--- core/scripts/test_all.sh
+++ core/scripts/test_all.sh
@@ -5,6 +5,10 @@
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -v|--verbose) verbose=1; shift ;;
+        -u|--unit) unit=1; shift ;;
+        -i|--integration) integration=1; shift ;;
+        --device) device=1; shift ;;
+        -h|--help) echo "Usage: $0 [-v] [-u] [-i] [--device]"; exit 0 ;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
@@ -20,4 +24,20 @@ if [ $verbose ]; then
     OPTIONS+=("--log-cli-level=DEBUG")
 fi
 
-poetry run coverage run -m pytest tests/ --log-level=DEBUG --device ${OPTIONS[@]} && poetry run coverage report -m
+if [ $device ]; then
+    OPTIONS+=("--device")
+fi
+
+TEST_DIRS=()
+
+if [ $unit ]; then
+    TEST_DIRS+=("tests/unit")
+elif [ $integration ]; then
+    TEST_DIRS+=("tests/integration")
+fi
+
+if [ ${#TEST_DIRS[@]} -eq 0 ]; then
+    TEST_DIRS+=("tests/unit" "tests/integration")
+fi
+
+poetry run coverage run -m pytest ${TEST_DIRS[@]} --log-level=DEBUG ${OPTIONS[@]} && poetry run coverage report -m
