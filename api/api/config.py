73a10015e96e25b8256742c6b9f2b123729d5984
Marvin Willms <marvin.willms@code.berlin>
2025-01-14 14:35:20 +0100

    feat(api): add api config

---
+++ api/api/config.py
@@ -0,0 +1,30 @@
+from typing import Literal
+
+from pydantic import (
+    computed_field,
+)
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_ignore_empty=False,
+        extra="ignore",
+    )
+    ENVIRONMENT: Literal["local", "staging", "production"] = "local"
+
+    PROJECT_NAME: str
+
+    SQLITE_PATH: str
+
+    # noinspection PyPep8Naming
+    @computed_field
+    @property
+    def SQLALCHEMY_DATABASE_URI(self) -> str:
+        return "sqlite:///" + self.SQLITE_PATH
+
+    EMAIL_RESET_TOKEN_EXPIRE_HOURS: int = 48
+
+
+settings = Settings()  # type: ignore
