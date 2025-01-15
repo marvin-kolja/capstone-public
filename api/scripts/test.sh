fcb6110f3d7626f74fc61b3a29fd6a03195b527b
Marvin Willms <marvin.willms@code.berlin>
2025-01-15 10:06:44 +0100

    chore(api): use & create tmp sqlite db for tests
    * Removes existing tmp test db, migrates to head using alembic (will create new db), and run tests passing the db path using the SQLITE_PATH env arg.

---
+++ api/scripts/test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Execute tests using a temporary SQLite database
+
+set -e
+set -x
+
+DB_PATH=test.db.sqlite3
+
+# Make sure we start clean
+rm -f $DB_PATH
+
+# Migrate the database to head before running tests
+SQLITE_PATH=$DB_PATH sh scripts/migrate.sh
+
+# Run the tests
+SQLITE_PATH=$DB_PATH coverage run -m pytest tests/
+
+# Clean up
+rm -f $DB_PATH
