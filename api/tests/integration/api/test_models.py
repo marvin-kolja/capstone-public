5aa93247927a29ef36c768299e23b6cb742936c1
Marvin Willms <marvin.willms@code.berlin>
2025-01-16 17:32:47 +0100

    test(api): make sure db cascade deletion works for test plan steps

---
+++ api/tests/integration/api/test_models.py
@@ -0,0 +1,62 @@
+from sqlalchemy import text
+from sqlmodel import select
+
+from api.models import SessionTestPlan, SessionTestPlanStep
+
+
+def test_session_test_plan_cascade_deletion(db):
+    """
+    GIVEN: A test plan with test steps
+
+    WHEN: The test plan is deleted not using SQLAlchemy ORM
+
+    THEN: The test cases are also deleted using SQL cascade deletion
+    """
+    # Create a test plan with test steps
+    test_plan = SessionTestPlan(
+        name="Test Plan 1",
+        xctestrun_path="path/to/xctestrun",
+        xctestrun_test_configuration="test_configuration",
+        repetitions=1,
+        repetition_strategy="entire_suite",
+    )
+    db.add(test_plan)
+    db.commit()
+    db.refresh(test_plan)
+
+    test_plan_id = test_plan.id
+
+    test_step_1 = SessionTestPlanStep(
+        order=0,
+        name="Test Step 1",
+        test_cases=["test/case/path"],
+        test_plan_id=test_plan.id,
+    )
+    test_step_2 = SessionTestPlanStep(
+        order=1,
+        name="Test Step 2",
+        test_cases=["test/case/path"],
+        test_plan_id=test_plan.id,
+    )
+    db.add(test_step_1)
+    db.add(test_step_2)
+    db.commit()
+
+    # Delete the test plan
+    # noinspection SqlResolve
+    db.execute(
+        text("DELETE FROM session_testplan WHERE id=:plan_id;"),
+        {"plan_id": test_plan_id.hex},
+    )
+
+    # Check if the test steps are deleted
+    db_plan = db.get(SessionTestPlan, test_plan_id)
+    assert db_plan is None
+
+    # Check if the test plan is deleted
+    db_steps = db.exec(
+        select(SessionTestPlanStep).where(
+            SessionTestPlanStep.test_plan_id == test_plan_id
+        )
+    ).all()
+    assert db_steps == []
