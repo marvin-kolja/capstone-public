a5462aabf002568a8faa62dcfc9cced378cb0b7b
Marvin Willms <marvin.willms@code.berlin>
2025-01-24 22:18:25 +0100

    feat(macOS): implement basic project detail view

--- macOS/Client/Client/Views/ProjectDetailView.swift
+++ macOS/Client/Client/Views/ProjectDetailView.swift
@@ -8,11 +8,52 @@
 import SwiftUI
 
 struct ProjectDetailView: View {
+    @EnvironmentObject var projectStore: ProjectStore
+    
     var body: some View {
-        Text("Detailed Project View")
+        VStack(alignment: .center) {
+            Text("Project information")
+                .font(.title3)
+            Divider()
+            Grid(alignment: .leading, horizontalSpacing: 16, verticalSpacing: 6) {
+                GridRow {
+                    Text("Name")
+                        .bold()
+                    Text(projectStore.project.name)
+                }
+                GridRow {
+                    Text("Path")
+                        .bold()
+                    Text(projectStore.project.path)
+                }
+                GridRow {
+                    Text("Configuration")
+                        .bold()
+                    Text(projectStore.project.configurations.map { resource in
+                        resource.name
+                    }.joined(separator: ", "))
+                }
+                GridRow {
+                    Text("Targets")
+                        .bold()
+                    Text(projectStore.project.targets.map { resource in
+                        resource.name
+                    }.joined(separator: ", "))
+                }
+                GridRow {
+                    Text("Schemes")
+                        .bold()
+                    Text(projectStore.project.schemes.map { resource in
+                        resource.name
+                    }.joined(separator: ", "))
+                }
+            }
+            Spacer()
+        }.padding()
     }
 }
 
 #Preview {
     ProjectDetailView()
+        .environmentObject(ProjectStore(project: Components.Schemas.XcProjectPublic.mock))
 }
