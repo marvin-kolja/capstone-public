eb534bce844467db91e6ede70913e603e4f08594
Marvin Willms <marvin.willms@code.berlin>
2025-01-27 19:32:09 +0100

    feat(macOS): computed vars if device is ready for builds/session, for convenience

--- macOS/Client/Client/Extensions/Device+Extensions.swift
+++ macOS/Client/Client/Extensions/Device+Extensions.swift
@@ -9,4 +9,47 @@ extension Components.Schemas.DeviceWithStatus {
     var isConnected: Bool {
         self.connected ?? false
     }
+    
+    
+    var isDeviceReadyForTestSession: Bool {
+        guard isDeviceReadyForBuilds else {
+            return false
+        }
+        
+        guard let status = status else {
+            return false
+        }
+        
+        // If tunnel connected is nil it indicates that the device does not need it.
+        guard status.tunnelConnected == nil || status.tunnelConnected == true else {
+            return false
+        }
+        
+        return true
+    }
+    
+    var isDeviceReadyForBuilds: Bool {
+        guard isConnected else {
+            return false
+        }
+        
+        guard let status = status else {
+            return false
+        }
+        
+        guard status.paired else {
+            return false
+        }
+        
+        // If developer mode is nil it indicates that the device does not need it.
+        guard status.developerModeEnabled == nil || status.developerModeEnabled == true else {
+            return false
+        }
+        
+        guard status.ddiMounted else {
+            return false
+        }
+        
+        return true
+    }
 }
