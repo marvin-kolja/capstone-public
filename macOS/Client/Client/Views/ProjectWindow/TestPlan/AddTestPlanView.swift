3d9466807cf87eaccc0df8085637c7625d779001
Marvin Willms <marvin.willms@code.berlin>
2025-01-27 20:07:14 +0100

    feat(macOS): implement `AddTestPlanView`

--- macOS/Client/Client/Views/ProjectWindow/TestPlan/AddTestPlanView.swift
+++ macOS/Client/Client/Views/ProjectWindow/TestPlan/AddTestPlanView.swift
@@ -8,20 +8,80 @@
 import SwiftUI
 
 struct AddTestPlanView: View {
+    @Environment(\.dismiss) private var dismiss
+    
+    @EnvironmentObject var buildStore: BuildStore
+    @EnvironmentObject var testPlanStore: TestPlanStore
+    
+    @State private var selectedBuildId: String?
+    
     var body: some View {
-
-        Text(
-            """
-            // TODO: 1. Select a build
-            // TODO: 2. Make sure device is ready
-            // TODO: 3. Get test cases
-            // TODO: 4. Show Test Plan Config Form
-            // TODO: 5. Show Test Plan Step Forms
-            """
-        )
+        VStack(spacing: 10) {
+            Text("Select a build to create a test plan")
+                .font(.title2)
+            Divider()
+            Picker("Build", selection: $selectedBuildId) {
+                ForEach(buildStore.builds, id: \.id) { build in
+                    Text(build.userFriendlyName)
+                        .tag(build.id)
+                }
+                Divider().tag(String?(nil))
+            }
+            
+            if let buildId = selectedBuildId, let build = buildStore.getBuildById(buildId: buildId) {
+                if !build.isBuildReadyForTests {
+                    Text("This build is not ready for tests!")
+                        .font(.headline)
+                        .foregroundStyle(.orange)
+                    Text("Try rebuilding & listing xc test cases...")
+                        .font(.subheadline)
+                        .foregroundStyle(.orange)
+                }
+                
+                BuildDetailView(build: build)
+                    .disabled(true)
+                    .overlay(Color.gray.opacity(0.2))
+            }
+            
+            LoadingButton(isLoading: testPlanStore.addingTestPlan) {
+                guard let buildId = selectedBuildId, let build = buildStore.getBuildById(buildId: buildId) else {
+                    return
+                }
+                
+                let data: Components.Schemas.SessionTestPlanCreate = .init(
+                    metrics: [],
+                    name: "Test Plan \(testPlanStore.testPlans.count)",
+                    projectId: testPlanStore.projectId,
+                    repetitionStrategy: .entireSuite,
+                    repetitions: 1,
+                    xcTestPlanName: build.testPlan
+                )
+                
+                Task {
+                    defer { dismiss() }
+                    
+                    await testPlanStore.add(data: data)
+                }
+            } label: {
+                Text("Add Test Plan")
+            }.disabled(!(getSelectedBuild?.isBuildReadyForTests ?? false))
+        }
+        .task { await buildStore.loadBuilds() }
+        .padding(20)
+    }
+    
+    var getSelectedBuild: Components.Schemas.BuildPublic? {
+        guard let buildId = selectedBuildId, let build = buildStore.getBuildById(buildId: buildId) else {
+            return nil
+        }
+        return build
     }
 }
 
 #Preview {
+    let projectId = Components.Schemas.XcProjectPublic.mock.id
+    
     AddTestPlanView()
+        .environmentObject(BuildStore(projectId: projectId, apiClient: MockAPIClient()))
+        .environmentObject(TestPlanStore(projectId: projectId, apiClient: MockAPIClient()))
 }
